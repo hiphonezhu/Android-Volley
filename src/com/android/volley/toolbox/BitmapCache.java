5fe9349da0b4cd3cb18e486943ef1eddd480f887
hiphonezhu <hiphonezhu@gmail.com>
2014-12-23 15:26:34 +0800

    lrucache bug fix

--- src/com/android/volley/toolbox/BitmapCache.java
+++ src/com/android/volley/toolbox/BitmapCache.java
@@ -1,31 +1,45 @@
 package com.android.volley.toolbox;
 
 import android.graphics.Bitmap;
+import android.os.Build;
 import android.util.LruCache;
 
 import com.android.volley.toolbox.ImageLoader.ImageCache;
 /**
- * Bitmap L1缓存
+ * Bitmap L1 Cache
  * @author hiphonezhu@gmail.com
  * @version [Volley, 2014-9-16]
  */
 public class BitmapCache implements ImageCache
 {
     private LruCache<String, Bitmap> mCache;  
-    
-    public BitmapCache() {  
-        // 获取到可用内存的最大值，使用内存超出这个值会引起OutOfMemory异常。  
-        // LruCache通过构造函数传入缓存值，以KB为单位。  
+    private static BitmapCache sInstance;
+    private BitmapCache() {  
         int maxMemory = (int) (Runtime.getRuntime().maxMemory() / 1024);  
-        // 使用最大可用内存值的1/8作为缓存的大小。  
         int cacheSize = maxMemory / 8;  
         mCache = new LruCache<String, Bitmap>(cacheSize) {  
             @Override  
             protected int sizeOf(String key, Bitmap bitmap) {  
-                return bitmap.getRowBytes() * bitmap.getHeight();  
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB)
+                {
+                    return bitmap.getByteCount() / 1024;
+                }
+                else
+                {
+                    return bitmap.getRowBytes() * bitmap.getHeight() / 1024;  
+                }
             }  
         };  
     }  
+    
+    public static synchronized BitmapCache getInstance()
+    {
+        if (sInstance == null)
+        {
+            sInstance = new BitmapCache();
+        }
+        return sInstance;
+    }
   
     @Override  
     public Bitmap getBitmap(String url) {  
